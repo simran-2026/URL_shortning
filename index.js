9e16809015b9a245164bd77d30cc3284b7a0f0a4
simran verma <vermasimran327@gmail.com>
2025-06-23 23:32:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const path= require('path');
-const staticRoute = require("./routes/staticrouter");
+const cookieParser = require('cookie-parser');
 const {connectMongodb}  = require("./connect");
+const{restrictToLoggedinUserOnly, checkAuth}= require('./middleware/auth');
+
+
 const URL = require("./models/url");
-const urlRoute = require("./routes/URL.JS");
+
+
+
+
+const urlRoute = require("./routes/url.js");
+const staticRoute = require("./routes/staticrouter");
+const userRoute = require("./routes/user");
+
+
 
 const app = express();
-
 const port = 8002 ;
 
 connectMongodb("mongodb://localhost:27017/short-url").then(()=> console.log("connectedto mongodb"));
@@ -18,11 +28,37 @@ app.set("views",path.resolve("./veiws"));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use("/", cookieParser());
+
+// app.get("/test",async(req,res)=>{
+//     const allurls = await URL.find({});
+//         return res.render("home",{
+
+//             urls: allurls,
 
 
 
-app.use ("/url", urlRoute);
-app.use("/", staticRoute);
+//         });
+    
+// });
+
+
+app.use ("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/",  checkAuth, staticRoute);
+app.use("/user",userRoute);
+// app.get('/:shortId', async (req,res) => {
+//     const shortId = req.params.shortId;
+//     const entry = await URL.findOneAndUpdate(
+//         {
+//         shortId, },
+//         {
+//             $push: {
+//             visitHistory:  {timestamp: Date.now()},
+//         },
+//         } 
+//    );
+//     res.redirect(entry.redirectURL);
+// })
 
 app.get('/:shortId', async (req, res) => {
     try {
